1b1bd676e8f9dd09eb03716e21e8dfadfc33b287
Fr0sk <the.fr0sk@gmail.com>
2017-02-23 19:05:28 +0000

    Semi-finished getData()

--- Source.cpp
+++ Source.cpp
@@ -12,19 +12,19 @@ bool getData(vector<int> &videos) {
 	file.open("me_at_the_zoo.in");
 	if (file.is_open()) {
 		string info;
-		
+
 		// Gets first line information
 		getline(file, info);
 		int i_videos = atoi(info.substr(0, info.find(" ")).c_str());
 		info = info.substr(info.find(" ") + 1);
 		int i_endpoints = atoi(info.substr(0, info.find(" ")).c_str());
 		info = info.substr(info.find(" ") + 1);
-		int i_request = atoi(info.substr(0, info.find(" ")).c_str());
+		int i_requestDescriptions = atoi(info.substr(0, info.find(" ")).c_str());
 		info = info.substr(info.find(" ") + 1);
 		int i_caches = atoi(info.substr(0, info.find(" ")).c_str());
 		info = info.substr(info.find(" ") + 1);
 		int i_cacheSize = atoi(info.substr(0, info.find(" ")).c_str());
-		
+
 		// Get videos sizes
 		getline(file, info);
 		for (int i = 0; i < i_videos; i++) {
@@ -33,29 +33,35 @@ bool getData(vector<int> &videos) {
 				info = info.substr(info.find(" ") + 1);
 		}
 
-		// 
-		getline(file, info);
-		for (int i = 0; i < i_videos; i++) {
-			videos.push_back(atoi(info.substr(0, info.find(" ")).c_str()));
-			if (i < i_videos + 1)
+		// Get endpoints data
+		for (int ep = 0; ep < i_endpoints; ep++) {
+			getline(file, info);
+			int datacenterLatency = atoi(info.substr(0, info.find(" ")).c_str());
+			info = info.substr(info.find(" ") + 1);
+			int numCacheServers = atoi(info.substr(0, info.find(" ")).c_str());
+
+			// Get latency for each cache server
+			for (int cs = 0; cs < numCacheServers; cs++) {
+				getline(file, info);
+				int cacheServerIndex = atoi(info.substr(0, info.find(" ")).c_str());
 				info = info.substr(info.find(" ") + 1);
+				int cacheServerLatency = atoi(info.substr(0, info.find(" ")).c_str());
+			}
 		}
 
+		// Get video request descriptions
+		for (int rd = 0; rd < i_requestDescriptions; rd++) {
+			getline(file, info);
+			int videoIdx = atoi(info.substr(0, info.find(" ")).c_str());
+			info = info.substr(info.find(" ") + 1);
+			int endpointIdx = atoi(info.substr(0, info.find(" ")).c_str());
+			info = info.substr(info.find(" ") + 1);
+			int numRequests = atoi(info.substr(0, info.find(" ")).c_str());
+		}
 
 		file.close();
 		return true;
 	}
-	
-	
-	//info = info.substr(info.substr(info.find(" ") + 1));
-
-
-	/*while (getline(file, line)) {
-		cout << i++ <<" - " << line << endl;
-	}*/
-
-	return false;	
-}
 
 class Endpoint {
 	map <int, int> video_requests;	// key = video, value = requests
